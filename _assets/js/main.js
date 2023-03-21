3d921dc38800ab225e3e1306f7150cfdfec5b0b3
CarloBattista <carlitobatti@gmail.com>
2023-03-21 13:59:08 +0100

    link css + password visibile in console + in HTML

---
+++ _assets/js/main.js
@@ -0,0 +1,22 @@
+/* 
+
+01-Chiedi all’utente il suo nome
+02-poi chiedi il suo cognome
+03-poi chiedi il suo colore preferito
+04-Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+*/
+
+let nome = prompt("Come ti chiami?")
+
+let cognome = prompt("Qual'è il tuo cognome?")
+
+let colorePreferito = prompt("Qual'è il tuo colore preferito?")
+
+const numero = 21;
+
+console.log(`${nome}${cognome}${colorePreferito}${numero}`)
+
+document.getElementById("password_non_sicura").innerHTML = `
+    Questa è la tua password non sicura: <span>${nome}${cognome}${colorePreferito}${numero}</span>
+`;
